a6fea0766f703fe5098ea3b4b6121cc9ae2476de
vanibm15 <vanibm15@gmail.com>
2022-06-15 23:50:23 +0530

    Added:phoneno_address_dept

--- src/main/java/com/bridgelabz/EmpRoll2.java
+++ src/main/java/com/bridgelabz/EmpRoll2.java
@@ -18,18 +18,22 @@ public class EmpRoll2 {
             System.out.println("connecting to database:" + jdbcURL);
             con = DriverManager.getConnection(jdbcURL, username, password);
             System.out.println("connection done successful!!" + con);
-            Statement statement=con.createStatement();
-            ResultSet resultSet =statement.executeQuery("select Gender,sum(salary) as sum," +
-                    "avg(salary) \n" +
-                    "as average,max(salary) as maximum,min(salary) as minimum\n" +
-                    "from employee group by Gender");
-            while(resultSet.next()){
-                System.out.println("id:"+resultSet.getInt("id"));
-                System.out.println("name:"+resultSet.getString("name"));
-                System.out.println("salary:"+resultSet.getDouble("salary"));
-                System.out.println("department:"+resultSet.getString("department"));
-                System.out.println("Joining_Date:"+resultSet.getDate("Joining_Date"));
-                System.out.println("Gender:"+resultSet.getString("Gender"));
+            Statement statement = con.createStatement();
+            statement.executeUpdate("alter table employee add column phone varchar(30);");
+            statement.executeUpdate("alter table employee add column address varchar(60);");
+
+            ResultSet resultSet = statement.executeQuery("  select * from employee");
+            while (resultSet.next()) {
+                System.out.println("id:" + resultSet.getInt("id"));
+                System.out.println("name:" + resultSet.getString("name"));
+                System.out.println("salary:" + resultSet.getDouble("salary"));
+                System.out.println("department:" + resultSet.getString("department"));
+                System.out.println("Joining_Date:" + resultSet.getDate("Joining_Date"));
+                System.out.println("Gender:" + resultSet.getString("Gender"));
+                System.out.println("phone:" + resultSet.getInt("phone"));
+                System.out.println("address:" + resultSet.getString("address"));
+
+
             }
         } catch (Exception e) {
             //  e.printStackTrace();
